2012b626a75db8345a0ffe8d6e462ad3333f66df
TuanLV <tuanlv5490@gmail.com>
2015-12-31 01:20:04 +0700

    Add login, log out.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
-]
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+)
